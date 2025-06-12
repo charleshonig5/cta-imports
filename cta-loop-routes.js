42be0ad8a2ffab0da3e9ac2d599dd725dfebfdcc
charleshonig5 <42223843+charleshonig5@users.noreply.github.com>
2025-06-12 16:46:34 +0000

    Add CTA loop route data for distance calculations

---
+++ cta-loop-routes.js
@@ -0,0 +1,926 @@
+// CTA Loop Route Data
+// Generated from GTFS stop-to-stop calculations
+// Date: 2025-06-12T16:38:00.614Z
+// Note: Distances are underestimated by ~10% for trains, ~20% for buses
+
+const loopRouteData = {
+  "1": {
+    "name": "Bronzeville/Union Station",
+    "totalDistanceKm": 7.2,
+    "totalMinutes": 33,
+    "stopCount": 33,
+    "type": "bus"
+  },
+  "2": {
+    "name": "Hyde Park Express",
+    "totalDistanceKm": 15.5,
+    "totalMinutes": 49,
+    "stopCount": 43,
+    "type": "bus"
+  },
+  "3": {
+    "name": "King Drive",
+    "totalDistanceKm": 21,
+    "totalMinutes": 77,
+    "stopCount": 105,
+    "type": "bus"
+  },
+  "4": {
+    "name": "Cottage Grove",
+    "totalDistanceKm": 24.7,
+    "totalMinutes": 86,
+    "stopCount": 122,
+    "type": "bus"
+  },
+  "5": {
+    "name": "South Shore Night Bus",
+    "totalDistanceKm": 19,
+    "totalMinutes": 49,
+    "stopCount": 105,
+    "type": "bus"
+  },
+  "6": {
+    "name": "Jackson Park Express",
+    "totalDistanceKm": 18.9,
+    "totalMinutes": 44,
+    "stopCount": 57,
+    "type": "bus"
+  },
+  "7": {
+    "name": "Harrison",
+    "totalDistanceKm": 5.3,
+    "totalMinutes": 15,
+    "stopCount": 27,
+    "type": "bus"
+  },
+  "8": {
+    "name": "Halsted",
+    "totalDistanceKm": 22.3,
+    "totalMinutes": 76,
+    "stopCount": 103,
+    "type": "bus"
+  },
+  "9": {
+    "name": "Ashland",
+    "totalDistanceKm": 27.1,
+    "totalMinutes": 71,
+    "stopCount": 96,
+    "type": "bus"
+  },
+  "11": {
+    "name": "Lincoln",
+    "totalDistanceKm": 7.1,
+    "totalMinutes": 27,
+    "stopCount": 35,
+    "type": "bus"
+  },
+  "12": {
+    "name": "Roosevelt",
+    "totalDistanceKm": 13.4,
+    "totalMinutes": 48,
+    "stopCount": 62,
+    "type": "bus"
+  },
+  "15": {
+    "name": "Jeffery Local",
+    "totalDistanceKm": 17.7,
+    "totalMinutes": 45,
+    "stopCount": 85,
+    "type": "bus"
+  },
+  "18": {
+    "name": "16th/18th",
+    "totalDistanceKm": 11.9,
+    "totalMinutes": 50,
+    "stopCount": 51,
+    "type": "bus"
+  },
+  "20": {
+    "name": "Madison",
+    "totalDistanceKm": 12.3,
+    "totalMinutes": 47,
+    "stopCount": 61,
+    "type": "bus"
+  },
+  "21": {
+    "name": "Cermak",
+    "totalDistanceKm": 12.9,
+    "totalMinutes": 50,
+    "stopCount": 55,
+    "type": "bus"
+  },
+  "22": {
+    "name": "Clark",
+    "totalDistanceKm": 17.4,
+    "totalMinutes": 74,
+    "stopCount": 81,
+    "type": "bus"
+  },
+  "24": {
+    "name": "Wentworth",
+    "totalDistanceKm": 15.6,
+    "totalMinutes": 48,
+    "stopCount": 55,
+    "type": "bus"
+  },
+  "26": {
+    "name": "South Shore Express",
+    "totalDistanceKm": 29.4,
+    "totalMinutes": 75,
+    "stopCount": 79,
+    "type": "bus"
+  },
+  "28": {
+    "name": "Stony Island",
+    "totalDistanceKm": 11.4,
+    "totalMinutes": 33,
+    "stopCount": 51,
+    "type": "bus"
+  },
+  "29": {
+    "name": "State",
+    "totalDistanceKm": 20.3,
+    "totalMinutes": 59,
+    "stopCount": 97,
+    "type": "bus"
+  },
+  "30": {
+    "name": "South Chicago",
+    "totalDistanceKm": 19.5,
+    "totalMinutes": 44,
+    "stopCount": 82,
+    "type": "bus"
+  },
+  "31": {
+    "name": "31st",
+    "totalDistanceKm": 6.4,
+    "totalMinutes": 26,
+    "stopCount": 27,
+    "type": "bus"
+  },
+  "34": {
+    "name": "South Michigan",
+    "totalDistanceKm": 10,
+    "totalMinutes": 32,
+    "stopCount": 50,
+    "type": "bus"
+  },
+  "35": {
+    "name": "31st/35th",
+    "totalDistanceKm": 14.3,
+    "totalMinutes": 48,
+    "stopCount": 65,
+    "type": "bus"
+  },
+  "36": {
+    "name": "Broadway",
+    "totalDistanceKm": 15.6,
+    "totalMinutes": 75,
+    "stopCount": 75,
+    "type": "bus"
+  },
+  "37": {
+    "name": "Sedgwick",
+    "totalDistanceKm": 7.1,
+    "totalMinutes": 29,
+    "stopCount": 31,
+    "type": "bus"
+  },
+  "39": {
+    "name": "Pershing",
+    "totalDistanceKm": 13,
+    "totalMinutes": 42,
+    "stopCount": 60,
+    "type": "bus"
+  },
+  "43": {
+    "name": "43rd",
+    "totalDistanceKm": 5.9,
+    "totalMinutes": 20,
+    "stopCount": 29,
+    "type": "bus"
+  },
+  "44": {
+    "name": "Wallace/Racine",
+    "totalDistanceKm": 14.9,
+    "totalMinutes": 42,
+    "stopCount": 75,
+    "type": "bus"
+  },
+  "47": {
+    "name": "47th",
+    "totalDistanceKm": 15.7,
+    "totalMinutes": 53,
+    "stopCount": 75,
+    "type": "bus"
+  },
+  "48": {
+    "name": "South Damen",
+    "totalDistanceKm": 9,
+    "totalMinutes": 25,
+    "stopCount": 43,
+    "type": "bus"
+  },
+  "49": {
+    "name": "Western",
+    "totalDistanceKm": 25.3,
+    "totalMinutes": 74,
+    "stopCount": 82,
+    "type": "bus"
+  },
+  "50": {
+    "name": "Damen",
+    "totalDistanceKm": 18.5,
+    "totalMinutes": 57,
+    "stopCount": 85,
+    "type": "bus"
+  },
+  "51": {
+    "name": "51st",
+    "totalDistanceKm": 7.3,
+    "totalMinutes": 22,
+    "stopCount": 34,
+    "type": "bus"
+  },
+  "52": {
+    "name": "Kedzie",
+    "totalDistanceKm": 13.1,
+    "totalMinutes": 53,
+    "stopCount": 64,
+    "type": "bus"
+  },
+  "53": {
+    "name": "Pulaski",
+    "totalDistanceKm": 17.1,
+    "totalMinutes": 54,
+    "stopCount": 86,
+    "type": "bus"
+  },
+  "54": {
+    "name": "Cicero",
+    "totalDistanceKm": 12.6,
+    "totalMinutes": 41,
+    "stopCount": 64,
+    "type": "bus"
+  },
+  "55": {
+    "name": "Garfield",
+    "totalDistanceKm": 14.1,
+    "totalMinutes": 49,
+    "stopCount": 69,
+    "type": "bus"
+  },
+  "56": {
+    "name": "Milwaukee",
+    "totalDistanceKm": 15.8,
+    "totalMinutes": 46,
+    "stopCount": 65,
+    "type": "bus"
+  },
+  "57": {
+    "name": "Laramie",
+    "totalDistanceKm": 5.8,
+    "totalMinutes": 20,
+    "stopCount": 29,
+    "type": "bus"
+  },
+  "59": {
+    "name": "59th/61st",
+    "totalDistanceKm": 14.7,
+    "totalMinutes": 48,
+    "stopCount": 73,
+    "type": "bus"
+  },
+  "60": {
+    "name": "Blue Island/26th",
+    "totalDistanceKm": 13.8,
+    "totalMinutes": 57,
+    "stopCount": 58,
+    "type": "bus"
+  },
+  "62": {
+    "name": "Archer",
+    "totalDistanceKm": 21.3,
+    "totalMinutes": 54,
+    "stopCount": 99,
+    "type": "bus"
+  },
+  "63": {
+    "name": "63rd",
+    "totalDistanceKm": 14.8,
+    "totalMinutes": 49,
+    "stopCount": 73,
+    "type": "bus"
+  },
+  "65": {
+    "name": "Grand",
+    "totalDistanceKm": 16.8,
+    "totalMinutes": 50,
+    "stopCount": 76,
+    "type": "bus"
+  },
+  "66": {
+    "name": "Chicago",
+    "totalDistanceKm": 15,
+    "totalMinutes": 48,
+    "stopCount": 69,
+    "type": "bus"
+  },
+  "67": {
+    "name": "67th/69th/71st",
+    "totalDistanceKm": 15.4,
+    "totalMinutes": 52,
+    "stopCount": 85,
+    "type": "bus"
+  },
+  "68": {
+    "name": "Northwest Highway",
+    "totalDistanceKm": 7.6,
+    "totalMinutes": 15,
+    "stopCount": 36,
+    "type": "bus"
+  },
+  "70": {
+    "name": "Division",
+    "totalDistanceKm": 4,
+    "totalMinutes": 10,
+    "stopCount": 21,
+    "type": "bus"
+  },
+  "71": {
+    "name": "71st/South Shore",
+    "totalDistanceKm": 15.1,
+    "totalMinutes": 44,
+    "stopCount": 76,
+    "type": "bus"
+  },
+  "72": {
+    "name": "North",
+    "totalDistanceKm": 6.5,
+    "totalMinutes": 22,
+    "stopCount": 31,
+    "type": "bus"
+  },
+  "73": {
+    "name": "Armitage",
+    "totalDistanceKm": 2.6,
+    "totalMinutes": 9,
+    "stopCount": 16,
+    "type": "bus"
+  },
+  "74": {
+    "name": "Fullerton",
+    "totalDistanceKm": 14,
+    "totalMinutes": 39,
+    "stopCount": 68,
+    "type": "bus"
+  },
+  "75": {
+    "name": "74th/75th",
+    "totalDistanceKm": 12.2,
+    "totalMinutes": 43,
+    "stopCount": 66,
+    "type": "bus"
+  },
+  "76": {
+    "name": "Diversey",
+    "totalDistanceKm": 14.7,
+    "totalMinutes": 59,
+    "stopCount": 72,
+    "type": "bus"
+  },
+  "77": {
+    "name": "Belmont",
+    "totalDistanceKm": 17,
+    "totalMinutes": 54,
+    "stopCount": 82,
+    "type": "bus"
+  },
+  "78": {
+    "name": "Montrose",
+    "totalDistanceKm": 14.3,
+    "totalMinutes": 50,
+    "stopCount": 68,
+    "type": "bus"
+  },
+  "79": {
+    "name": "79th",
+    "totalDistanceKm": 8,
+    "totalMinutes": 35,
+    "stopCount": 40,
+    "type": "bus"
+  },
+  "80": {
+    "name": "Irving Park",
+    "totalDistanceKm": 14.3,
+    "totalMinutes": 47,
+    "stopCount": 73,
+    "type": "bus"
+  },
+  "81": {
+    "name": "Lawrence",
+    "totalDistanceKm": 10.6,
+    "totalMinutes": 44,
+    "stopCount": 49,
+    "type": "bus"
+  },
+  "82": {
+    "name": "Kimball/Homan",
+    "totalDistanceKm": 18.7,
+    "totalMinutes": 67,
+    "stopCount": 94,
+    "type": "bus"
+  },
+  "84": {
+    "name": "Peterson",
+    "totalDistanceKm": 10.2,
+    "totalMinutes": 37,
+    "stopCount": 45,
+    "type": "bus"
+  },
+  "85": {
+    "name": "Central",
+    "totalDistanceKm": 13.2,
+    "totalMinutes": 42,
+    "stopCount": 67,
+    "type": "bus"
+  },
+  "86": {
+    "name": "Narragansett/Ridgeland",
+    "totalDistanceKm": 13.5,
+    "totalMinutes": 40,
+    "stopCount": 64,
+    "type": "bus"
+  },
+  "87": {
+    "name": "87th",
+    "totalDistanceKm": 3,
+    "totalMinutes": 8,
+    "stopCount": 14,
+    "type": "bus"
+  },
+  "88": {
+    "name": "Higgins",
+    "totalDistanceKm": 6.9,
+    "totalMinutes": 16,
+    "stopCount": 34,
+    "type": "bus"
+  },
+  "90": {
+    "name": "Harlem",
+    "totalDistanceKm": 10.9,
+    "totalMinutes": 39,
+    "stopCount": 57,
+    "type": "bus"
+  },
+  "91": {
+    "name": "Austin",
+    "totalDistanceKm": 12.9,
+    "totalMinutes": 37,
+    "stopCount": 66,
+    "type": "bus"
+  },
+  "92": {
+    "name": "Foster",
+    "totalDistanceKm": 9.6,
+    "totalMinutes": 25,
+    "stopCount": 47,
+    "type": "bus"
+  },
+  "93": {
+    "name": "California/Dodge",
+    "totalDistanceKm": 12,
+    "totalMinutes": 40,
+    "stopCount": 58,
+    "type": "bus"
+  },
+  "94": {
+    "name": "California",
+    "totalDistanceKm": 25.7,
+    "totalMinutes": 87,
+    "stopCount": 122,
+    "type": "bus"
+  },
+  "95": {
+    "name": "95th",
+    "totalDistanceKm": 15,
+    "totalMinutes": 42,
+    "stopCount": 72,
+    "type": "bus"
+  },
+  "96": {
+    "name": "Lunt",
+    "totalDistanceKm": 5.9,
+    "totalMinutes": 19,
+    "stopCount": 27,
+    "type": "bus"
+  },
+  "97": {
+    "name": "Skokie",
+    "totalDistanceKm": 12.9,
+    "totalMinutes": 36,
+    "stopCount": 61,
+    "type": "bus"
+  },
+  "100": {
+    "name": "Jeffery Manor Express",
+    "totalDistanceKm": 11.3,
+    "totalMinutes": 28,
+    "stopCount": 48,
+    "type": "bus"
+  },
+  "103": {
+    "name": "West 103rd",
+    "totalDistanceKm": 10.5,
+    "totalMinutes": 21,
+    "stopCount": 55,
+    "type": "bus"
+  },
+  "106": {
+    "name": "East 103rd",
+    "totalDistanceKm": 5.3,
+    "totalMinutes": 16,
+    "stopCount": 26,
+    "type": "bus"
+  },
+  "108": {
+    "name": "Halsted/95th",
+    "totalDistanceKm": 8.3,
+    "totalMinutes": 22,
+    "stopCount": 42,
+    "type": "bus"
+  },
+  "111": {
+    "name": "111th/King Drive",
+    "totalDistanceKm": 10.5,
+    "totalMinutes": 25,
+    "stopCount": 45,
+    "type": "bus"
+  },
+  "112": {
+    "name": "Vincennes/111th",
+    "totalDistanceKm": 10.4,
+    "totalMinutes": 25,
+    "stopCount": 47,
+    "type": "bus"
+  },
+  "115": {
+    "name": "Pullman/115th",
+    "totalDistanceKm": 11.1,
+    "totalMinutes": 32,
+    "stopCount": 54,
+    "type": "bus"
+  },
+  "119": {
+    "name": "Michigan/119th",
+    "totalDistanceKm": 10.2,
+    "totalMinutes": 35,
+    "stopCount": 49,
+    "type": "bus"
+  },
+  "120": {
+    "name": "Ogilvie/Streeterville Express",
+    "totalDistanceKm": 2.6,
+    "totalMinutes": 16,
+    "stopCount": 6,
+    "type": "bus"
+  },
+  "121": {
+    "name": "Union/Streeterville Express",
+    "totalDistanceKm": 2.8,
+    "totalMinutes": 18,
+    "stopCount": 7,
+    "type": "bus"
+  },
+  "124": {
+    "name": "Navy Pier",
+    "totalDistanceKm": 3.9,
+    "totalMinutes": 16,
+    "stopCount": 16,
+    "type": "bus"
+  },
+  "125": {
+    "name": "Water Tower Express",
+    "totalDistanceKm": 4.1,
+    "totalMinutes": 33,
+    "stopCount": 17,
+    "type": "bus"
+  },
+  "126": {
+    "name": "Jackson",
+    "totalDistanceKm": 13.2,
+    "totalMinutes": 46,
+    "stopCount": 63,
+    "type": "bus"
+  },
+  "134": {
+    "name": "Stockton/LaSalle Express",
+    "totalDistanceKm": 8.1,
+    "totalMinutes": 40,
+    "stopCount": 18,
+    "type": "bus"
+  },
+  "135": {
+    "name": "Clarendon/LaSalle Express",
+    "totalDistanceKm": 11.1,
+    "totalMinutes": 55,
+    "stopCount": 27,
+    "type": "bus"
+  },
+  "136": {
+    "name": "Sheridan/LaSalle Express",
+    "totalDistanceKm": 15.3,
+    "totalMinutes": 56,
+    "stopCount": 38,
+    "type": "bus"
+  },
+  "143": {
+    "name": "Stockton/Michigan Express",
+    "totalDistanceKm": 7.5,
+    "totalMinutes": 35,
+    "stopCount": 23,
+    "type": "bus"
+  },
+  "146": {
+    "name": "Inner Lake Shore/Michigan Express",
+    "totalDistanceKm": 13.1,
+    "totalMinutes": 49,
+    "stopCount": 39,
+    "type": "bus"
+  },
+  "147": {
+    "name": "Outer DuSable Lake Shore Express",
+    "totalDistanceKm": 17.8,
+    "totalMinutes": 59,
+    "stopCount": 46,
+    "type": "bus"
+  },
+  "148": {
+    "name": "Clarendon/Michigan Express",
+    "totalDistanceKm": 12,
+    "totalMinutes": 48,
+    "stopCount": 30,
+    "type": "bus"
+  },
+  "151": {
+    "name": "Sheridan",
+    "totalDistanceKm": 16.9,
+    "totalMinutes": 61,
+    "stopCount": 89,
+    "type": "bus"
+  },
+  "152": {
+    "name": "Addison",
+    "totalDistanceKm": 16.1,
+    "totalMinutes": 49,
+    "stopCount": 80,
+    "type": "bus"
+  },
+  "155": {
+    "name": "Devon",
+    "totalDistanceKm": 6.2,
+    "totalMinutes": 29,
+    "stopCount": 33,
+    "type": "bus"
+  },
+  "156": {
+    "name": "LaSalle",
+    "totalDistanceKm": 8.5,
+    "totalMinutes": 33,
+    "stopCount": 44,
+    "type": "bus"
+  },
+  "157": {
+    "name": "Streeterville/Taylor",
+    "totalDistanceKm": 12.7,
+    "totalMinutes": 70,
+    "stopCount": 56,
+    "type": "bus"
+  },
+  "165": {
+    "name": "West 65th",
+    "totalDistanceKm": 6.3,
+    "totalMinutes": 18,
+    "stopCount": 30,
+    "type": "bus"
+  },
+  "169": {
+    "name": "69th/UPS Express",
+    "totalDistanceKm": 23.4,
+    "totalMinutes": 42,
+    "stopCount": 10,
+    "type": "bus"
+  },
+  "171": {
+    "name": "U. of Chicago/Hyde Park",
+    "totalDistanceKm": 3.8,
+    "totalMinutes": 12,
+    "stopCount": 15,
+    "type": "bus"
+  },
+  "172": {
+    "name": "U. of Chicago/Kenwood",
+    "totalDistanceKm": 3.5,
+    "totalMinutes": 17,
+    "stopCount": 15,
+    "type": "bus"
+  },
+  "192": {
+    "name": "University of Chicago Hospitals Express",
+    "totalDistanceKm": 11.3,
+    "totalMinutes": 34,
+    "stopCount": 6,
+    "type": "bus"
+  },
+  "201": {
+    "name": "Central/Ridge",
+    "totalDistanceKm": 13.4,
+    "totalMinutes": 44,
+    "stopCount": 60,
+    "type": "bus"
+  },
+  "206": {
+    "name": "Evanston Circulator",
+    "totalDistanceKm": 9.9,
+    "totalMinutes": 30,
+    "stopCount": 48,
+    "type": "bus"
+  },
+  "Red": {
+    "name": "Red Line",
+    "totalDistanceKm": 34.4,
+    "totalMinutes": 62,
+    "stopCount": 30,
+    "type": "train"
+  },
+  "Org": {
+    "name": "Orange Line",
+    "totalDistanceKm": 33.3,
+    "totalMinutes": 58,
+    "stopCount": 24,
+    "type": "train"
+  },
+  "Brn": {
+    "name": "Brown Line",
+    "totalDistanceKm": 30.3,
+    "totalMinutes": 81,
+    "stopCount": 46,
+    "type": "train"
+  },
+  "Blue": {
+    "name": "Blue Line",
+    "totalDistanceKm": 29.4,
+    "totalMinutes": 50,
+    "stopCount": 23,
+    "type": "train"
+  },
+  "G": {
+    "name": "Green Line",
+    "totalDistanceKm": 29.2,
+    "totalMinutes": 61,
+    "stopCount": 29,
+    "type": "train"
+  },
+  "Pink": {
+    "name": "Pink Line",
+    "totalDistanceKm": 29,
+    "totalMinutes": 62,
+    "stopCount": 36,
+    "type": "train"
+  },
+  "Y": {
+    "name": "Yellow Line",
+    "totalDistanceKm": 7.7,
+    "totalMinutes": 8,
+    "stopCount": 3,
+    "type": "train"
+  },
+  "P": {
+    "name": "Purple Line",
+    "totalDistanceKm": 24,
+    "totalMinutes": 55,
+    "stopCount": 27,
+    "type": "train"
+  },
+  "X9": {
+    "name": "Ashland Express",
+    "totalDistanceKm": 27.3,
+    "totalMinutes": 88,
+    "stopCount": 38,
+    "type": "bus"
+  },
+  "X49": {
+    "name": "Western Express",
+    "totalDistanceKm": 25.3,
+    "totalMinutes": 79,
+    "stopCount": 36,
+    "type": "bus"
+  },
+  "J14": {
+    "name": "Jeffery Jump",
+    "totalDistanceKm": 24.5,
+    "totalMinutes": 53,
+    "stopCount": 33,
+    "type": "bus"
+  },
+  "X4": {
+    "name": "Cottage Grove Express",
+    "totalDistanceKm": 21.8,
+    "totalMinutes": 69,
+    "stopCount": 37,
+    "type": "bus"
+  },
+  "53A": {
+    "name": "South Pulaski",
+    "totalDistanceKm": 16.3,
+    "totalMinutes": 39,
+    "stopCount": 75,
+    "type": "bus"
+  },
+  "54A": {
+    "name": "North Cicero/Skokie Blvd.",
+    "totalDistanceKm": 15.8,
+    "totalMinutes": 50,
+    "stopCount": 67,
+    "type": "bus"
+  },
+  "52A": {
+    "name": "South Kedzie",
+    "totalDistanceKm": 14.8,
+    "totalMinutes": 42,
+    "stopCount": 69,
+    "type": "bus"
+  },
+  "54B": {
+    "name": "South Cicero",
+    "totalDistanceKm": 12.8,
+    "totalMinutes": 37,
+    "stopCount": 45,
+    "type": "bus"
+  },
+  "8A": {
+    "name": "South Halsted",
+    "totalDistanceKm": 11.3,
+    "totalMinutes": 31,
+    "stopCount": 58,
+    "type": "bus"
+  },
+  "62H": {
+    "name": "Archer/Harlem",
+    "totalDistanceKm": 9,
+    "totalMinutes": 21,
+    "stopCount": 42,
+    "type": "bus"
+  },
+  "81W": {
+    "name": "West Lawrence",
+    "totalDistanceKm": 8.5,
+    "totalMinutes": 20,
+    "stopCount": 40,
+    "type": "bus"
+  },
+  "85A": {
+    "name": "North Central",
+    "totalDistanceKm": 7,
+    "totalMinutes": 16,
+    "stopCount": 40,
+    "type": "bus"
+  },
+  "63W": {
+    "name": "West 63rd",
+    "totalDistanceKm": 6.9,
+    "totalMinutes": 18,
+    "stopCount": 35,
+    "type": "bus"
+  },
+  "49B": {
+    "name": "North Western",
+    "totalDistanceKm": 5.8,
+    "totalMinutes": 17,
+    "stopCount": 28,
+    "type": "bus"
+  },
+  "55N": {
+    "name": "55th/Narragansett",
+    "totalDistanceKm": 5.8,
+    "totalMinutes": 16,
+    "stopCount": 22,
+    "type": "bus"
+  },
+  "55A": {
+    "name": "55th/Austin",
+    "totalDistanceKm": 5.2,
+    "totalMinutes": 17,
+    "stopCount": 20,
+    "type": "bus"
+  },
+  "111A": {
+    "name": "Pullman Shuttle",
+    "totalDistanceKm": 4.4,
+    "totalMinutes": 19,
+    "stopCount": 13,
+    "type": "bus"
+  }
+};
+
+module.exports = loopRouteData;
